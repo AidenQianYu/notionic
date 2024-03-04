7c64aa7a202f76251687a4e9896ffd0266e226bf
AidenQianYu <113301606+AidenQianYu@users.noreply.github.com>
2024-03-04 15:53:58 +0800

    Update Logo.js

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 
 const Logo = (props) => (
   <svg
-    xmlns='file:///D:/Doc/Personal/Web/favicon.svg'
+    xmlns='http://www.w3.org/2000/svg'
     width='36'
     height='36'
     viewBox='0 0 100 100'
