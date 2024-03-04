40bce7e61c3422eb262854282fa92d99b57dc300
AidenQianYu <113301606+AidenQianYu@users.noreply.github.com>
2024-03-04 20:48:19 +0800

    Update Social.js

--- components/Common/Social.js
+++ components/Common/Social.js
@@ -5,29 +5,25 @@ const Social = () => {
   return (
     <div className='flex gap-4'>
 
-	 	 	<Link
-        // href={`${BLOG.socialLink.telegram}`}
-        passHref
-        href='/contact'
+	<Link
+        href={`${BLOG.socialLink.twitter}`}
         scroll={false}
         target='_blank'
-        aria-label='Mail'
+        aria-label='Twitter'
         className='text-gray-400 hover:text-gray-500 active:text-gray-600 transition duration-100'
       >
-
-        <svg 
-          xmlns="http://www.w3.org/2000/svg" 
-          fill="none" 
-          viewBox="0 0 24 24" 
-          stroke-width="1.5" 
-          stroke="currentColor" 
-          class="w-6 h-6"
+        <svg
+          className='w-5 h-5'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='currentColor'
+          xmlns='http://www.w3.org/2000/svg'
         >
-          <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+          <path fill='none' d='M0 0h24v24H0z' />
+          <path d='M15.3 5.55a2.9 2.9 0 0 0-2.9 2.847l-.028 1.575a.6.6 0 0 1-.68.583l-1.561-.212c-2.054-.28-4.022-1.226-5.91-2.799-.598 3.31.57 5.603 3.383 7.372l1.747 1.098a.6.6 0 0 1 .034.993L7.793 18.17c.947.059 1.846.017 2.592-.131 4.718-.942 7.855-4.492 7.855-10.348 0-.478-1.012-2.141-2.94-2.141zm-4.9 2.81a4.9 4.9 0 0 1 8.385-3.355c.711-.005 1.316.175 2.669-.645-.335 1.64-.5 2.352-1.214 3.331 0 7.642-4.697 11.358-9.463 12.309-3.268.652-8.02-.419-9.382-1.841.694-.054 3.514-.357 5.144-1.55C5.16 15.7-.329 12.47 3.278 3.786c1.693 1.977 3.41 3.323 5.15 4.037 1.158.475 1.442.465 1.973.538z' />
         </svg>
-
       </Link>
-    
       <Link
         href={`${BLOG.socialLink.github}`}
         scroll={false}
