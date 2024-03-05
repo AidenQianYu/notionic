9a3a44a30b02984fe9c6634f1dc5d768b4572e74
AQian <113301606+AidenQianYu@users.noreply.github.com>
2024-03-05 15:24:51 +0800

    Update Newsletter.js

--- components/Hero/Newsletter.js
+++ components/Hero/Newsletter.js
@@ -34,7 +34,9 @@ const NewsletterHero = ({ blockMap }) => {
               
         <div className='w-1/5'>
           <CubeTransparentIcon className='object-cover object-center text-gray-500 dark:text-gray-300' />
-        
+         </div>
+      </div>
+    </>
 
 
   )
